203ec65c080e8ff52dde663c62bc63082d20d662
Edennn <32844185+Edennn@users.noreply.github.com>
2018-02-21 17:52:33 +0100

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -69,14 +69,14 @@ function gestionarXml(dadesXml){
   respuestasCheckbox[i]=xmlDoc.getElementById("profe_003").getElementsByTagName("answer")[i].innerHTML;
  }
  //RADIO
- var tituloSelect1=xmlDoc.getElementsByTagName("title")[1].innerHTML;
+ var tituloSelect1=xmlDoc.getElementsByTagName("title")[3].innerHTML;
  var opcionesSelect1 = [];
- var nopt1 = xmlDoc.getElementById("profe_002").getElementsByTagName('option').length;
+ var nopt1 = xmlDoc.getElementById("profe_003").getElementsByTagName('option').length;
   for (i = 0; i < nopt; i++) { 
-    opcionesSelect1[i] = xmlDoc.getElementById("profe_002").getElementsByTagName('option')[i].innerHTML;
+    opcionesSelect1[i] = xmlDoc.getElementById("profe_003").getElementsByTagName('option')[i].innerHTML;
  }
  ponerDatosSelectHtml(tituloSelect1,opcionesSelect1);
- respuestaSelect1=parseInt(xmlDoc.getElementsByTagName("answer")[1].innerHTML);
+ respuestaSelect1=parseInt(xmlDoc.getElementsByTagName("answer")[3].innerHTML);
 }
 
 //****************************************************************************************************
